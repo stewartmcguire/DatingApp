537aa8bdf9d1b1876371fb2af383fb101493a190
Stewart McGuire <stewart.mcguire@arcadis.com>
2025-07-02 22:25:48 -0700

    Section 3 - Complete

--- API/Program.cs
+++ API/Program.cs
@@ -6,9 +6,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<AppDbContext>(options => {
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -19,6 +21,14 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.UseCors(policy =>
+{
+    policy.AllowAnyHeader()
+          .AllowAnyMethod()
+          .WithOrigins("http://localhost:4200", "https://localhost:4200")
+          .AllowCredentials();
+});
 
 // app.UseHttpsRedirection();
 
